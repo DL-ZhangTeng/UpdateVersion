5c8086b1992c4ff5da0ad20edc63574d9641bdae
Swing <763263311@qq.comm>
2022-08-20 14:21:11 +0800

    版本信息请求兼容https

--- updateversionlibrary/src/main/java/com/zhangteng/updateversionlibrary/utils/HttpRequest.java
+++ updateversionlibrary/src/main/java/com/zhangteng/updateversionlibrary/utils/HttpRequest.java
@@ -2,10 +2,16 @@ package com.zhangteng.updateversionlibrary.utils;
 
 import android.util.Log;
 
+import com.zhangteng.updateversionlibrary.UpdateVersion;
+import com.zhangteng.utils.SSLUtils;
+import com.zhangteng.utils.URLUtilsKt;
+
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.SocketTimeoutException;
 import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * @author swing
@@ -16,6 +22,10 @@ public class HttpRequest {
         try {
             URL urlPath = new URL(url);
             HttpURLConnection httpURLConnection = (HttpURLConnection) urlPath.openConnection();
+            if (URLUtilsKt.isHttpsUrl(url) && httpURLConnection instanceof HttpsURLConnection) {
+                ((HttpsURLConnection) httpURLConnection).setSSLSocketFactory(UpdateVersion.getSslParams().getSSLSocketFactory());
+                ((HttpsURLConnection) httpURLConnection).setHostnameVerifier(SSLUtils.INSTANCE.getUnSafeHostnameVerifier());
+            }
             httpURLConnection.setConnectTimeout(3000);
             httpURLConnection.setReadTimeout(3000);
 
