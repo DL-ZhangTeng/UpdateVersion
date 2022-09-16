aeb6e50857d1659d0befaa041d9d958b8451bc81
Swing <763263311@qq.comm>
2022-09-16 16:23:29 +0800

    兼容分区存储

--- updateversionlibrary/src/main/java/com/zhangteng/updateversionlibrary/config/Constant.java
+++ updateversionlibrary/src/main/java/com/zhangteng/updateversionlibrary/config/Constant.java
@@ -9,16 +9,10 @@ import java.util.HashMap;
  */
 public class Constant {
 
-    public static final String PATH = Environment
-            .getExternalStorageDirectory().getPath();
+    public static String PATH = Environment
+            .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
     public static final String SUFFIX = ".apk";
     public static final String APK_PATH = "APK_PATH";
     public static final String APP_NAME = "APP_NAME";
     public static HashMap<String, String> cache = new HashMap<String, String>();
-//    public static boolean IS_UPDATE_TEST = false;
-//    public static boolean IS_UPDATE_DOWNLOAD_WITH_BROWSER = false;
-//    public static boolean IS_AUTO_INSTALL = false;
-//    public static boolean IS_UPDATE_DIALOG_SHOW = false;
-//    public static boolean IS_HINT_VERSION = false;
-//    public static String CHECK_UPDATE_COMMON_URL = "";
 }
